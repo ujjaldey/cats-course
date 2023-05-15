1ad2cdeff5943d8739ae1b735488fa6053f043f2
Ujjal Dey <ujjaldey@gmail.com>
2023-05-15 23:23:25 +0800

    Data Validation, Part 2

--- src/main/scala/part3datamanipulation/DataValidation.scala
+++ src/main/scala/part3datamanipulation/DataValidation.scala
@@ -3,6 +3,7 @@ package part3datamanipulation
 import cats.Semigroup
 
 import scala.annotation.tailrec
+import scala.util.Try
 
 object DataValidation extends App {
 
@@ -65,5 +66,84 @@ object DataValidation extends App {
   println(validateNumber(-20))
   println(validateNumber(201))
 
+  println("====")
+
+  // chain
+  println(aValidValue.andThen(_ => anInvalidValue)) // flatMap will short-circuit the additional chain. hence we do not use flatMap
+  // test valid value
+  println(aValidValue.ensure(List("something went wrong"))(_ % 2 == 0))
+  // transform
+  println(aValidValue.map(_ + 1))
+  println(aValidValue.leftMap(_.length))
+  println(aValidValue.bimap(_.length, _ + 1))
+  println(aValidValue.bimap(_.length, _ + 1))
+  println("====")
+
+  // interoperate with stdlib
+  val eitherToValidated: Validated[List[String], Int] = Validated.fromEither(Right(42))
+  val optionToValidated: Validated[List[String], Int] = Validated.fromOption(None, List("nothing present here"))
+  val tryToValidated: Validated[Throwable, Int] = Validated.fromTry(Try("something".toInt))
+
+  println(eitherToValidated)
+  println(optionToValidated)
+  println(tryToValidated)
+  println("====")
+
+  // backwards
+  println(aValidValue.toOption)
+  println(aValidValue.toEither)
+  // nothing like toTry
+  println("====")
+
+  // TODO 2 - form valiation
+  object FormValidation {
+
+    import cats.instances.string._
+
+    type FormValidation[T] = Validated[List[String], T]
+
+    def getValue(form: Map[String, String], fieldName: String): FormValidation[String] =
+      Validated.fromOption(form.get(fieldName), List(s"The field $fieldName must be specified."))
+
+    def nonBlank(value: String, fieldName: String): FormValidation[String] =
+      Validated.cond(value.length > 0, value, List(s"The field $fieldName must not be blank."))
+
+    def emailProperForm(email: String): FormValidation[String] =
+      Validated.cond(email.contains("@"), email, List("Email is invalid."))
+
+    def passwordCheck(password: String): FormValidation[String] =
+      Validated.cond(password.length >= 10, password, List("Password must be at least 10 characters long."))
+
+    /*
+      fields are:
+        - name
+        - email
+        - password
+
+      rules (show all the validation errors):
+        - name, email, and password MUST be specified
+        - name must not be blank
+        - email must have "@"
+        - password must have >= characters
+     */
+    def validateForm(form: Map[String, String]): FormValidation[String] =
+      getValue(form, "Name").andThen(name => nonBlank(name, "Name"))
+        .combine(getValue(form, "Email").andThen(emailProperForm)) // requires import cats.instances.string._
+        .combine(getValue(form, "Password").andThen(passwordCheck)) // requires import cats.instances.string._
+        .map(_ => "User registration complete.")
+  }
+
+  val form = Map(
+    "Name" -> "ujjal",
+    "Email" -> "ujjal#gmail.com",
+    "Password" -> "abcdef34"
+  )
+  println(FormValidation.validateForm(form))
+  println("====")
+
+  import cats.syntax.validated._ // extension method
+
+  println(42.valid[List[String]]) // Validated[List[String], Int]
+  println("something wrong".invalid[List[String]]) // Validated[List[String], Int]
   println("====")
 }
