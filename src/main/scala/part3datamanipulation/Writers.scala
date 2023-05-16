14d46584710fff7d10afc9ba2d3a0a14fc49b2a4
Ujjal Dey <ujjaldey@gmail.com>
2023-05-17 00:21:52 +0800

    Re: Writers

--- src/main/scala/part3datamanipulation/Writers.scala
+++ src/main/scala/part3datamanipulation/Writers.scala
@@ -4,22 +4,27 @@ import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 
 object Writers extends App {
+  // Writers are another data manipulation tool in cats
+  // it is a data type in cats that will let you keep track of useful information while your data is being manipulated
 
   import cats.data.Writer
 
   // 1 - define them at the start
+  // List[String] is of logs type, Int is of value type
   val aWriter: Writer[List[String], Int] = Writer(List("started something"), 45)
+  println(aWriter)
   // Writer is a wrapper over some kind of value, like Int (here). can be String or anything else
   // and as you manipulate/transform the data, then you want to keep track of some sort of additional data like the sequence, or some sort of logs.
   // The second type of argument is called value type, the first type of argument is called log type.
 
   // 2 - manipulate them with pure FP
   val anIncreasedWriter = aWriter.map(_ + 1) // value increases, logs stay the same
-  val aLogsWriter = aWriter.mapWritten(_ :+ "found something interesting")
-  val aWriterWithBoth = aWriter.bimap(_ :+ "found something interesting", _ + 1)
-  val aWriterWithBoth2 = aWriter.mapBoth { (logs, value) =>
+  val aLogsWriter = aWriter.mapWritten(_ :+ "found something interesting") // value stays the same, logs change
+  val aWriterWithBoth = aWriter.bimap(_ :+ "found something interesting", _ + 1) // both value and logs change
+  val aWriterWithBoth2 = aWriter.mapBoth { (logs, value) => // both value and logs change. but here you can use the logs in value, and value in logs (different than bimap)
     (logs :+ "found something interesting", value + 1)
   }
+
   println(anIncreasedWriter)
   println(aLogsWriter)
   println(aWriterWithBoth)
@@ -30,9 +35,9 @@ object Writers extends App {
   // and at the end, you can dump either the value or the log.
 
   // 3 - dump either the value or the logs
-  val desiredValue = aWriter.value
-  val logs = aWriter.written
-  val (l, v) = aWriter.run
+  val desiredValue = aWriter.value // extract the value
+  val logs = aWriter.written // extract the log
+  val (l, v) = aWriter.run // extract both logs and value
 
   println(desiredValue)
   println(logs)
@@ -52,13 +57,14 @@ object Writers extends App {
     vb <- writerB
   } yield va + vb
 
-  println(compositeWriter.run)
+  println(compositeWriter.run) // appends both the logs and values. the logs and values will get combined using their natural combine function. i.e. sum for Int, and append for List[String]. for later we need to import the vector semigroup above
 
   // reset the logs
 
   import cats.instances.list._ // an implicit Monoid[List[Int]]
 
   val anEmptyWriter = aWriter.reset // clear the logs, keep the desired value. require import cats.instances.list._
+  // reset requires an empty value. hence we need an implicit of Monoid of that type (monoid has .empty method).
 
   // TODO 1: rewrite a function which prints things with writers
   def countAndSay(n: Int): Unit = {
@@ -78,9 +84,10 @@ object Writers extends App {
   def countAndLog(n: Int): Writer[Vector[String], Int] = {
     if (n <= 0) Writer(Vector("Starting!"), 0)
     else countAndLog(n - 1).flatMap(_ => Writer(Vector(s" $n"), n))
+    // instead of flatMap, bimap or mapBoth will also work
   }
 
-  countAndLog(10).written.foreach(print)
+  countAndLog(10).written.foreach(print) // written gets the logs
   println("====")
 
   // benefit #1: we work with pure FP
@@ -99,20 +106,21 @@ object Writers extends App {
   naiveSum(10)
   println("====")
 
+  // for logs, we use Vector, because Vector is faster for appending or combining
   def sumWithLogs(n: Int): Writer[Vector[String], Int] = {
     if (n <= 0) Writer(Vector(), 0)
     else for {
-      // first and third line with _ <- is unnecessary, they are just for the Writer appending
+      // first and third line with "_ <-" is unnecessary, they are just for the Writer appending
       _ <- Writer(Vector(s"Now at $n "), n)
       lowerSum <- sumWithLogs(n - 1)
       _ <- Writer(Vector(s"Computed sum(${n - 1}) = $lowerSum "), n)
     } yield lowerSum + n
   }
 
-  sumWithLogs(10).written.foreach(println)
+  sumWithLogs(10).written.foreach(println) // written gets the logs
   println("====")
 
-  // if we call naiveSum from 2 different threads, you dont know which log comes from which thread
+  // if we call naiveSum from 2 different threads, its logs are going to be interspersed. you dont know which log comes from which thread.
   // this is a bad aspect
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
   Future(naiveSum(10)).foreach(println)
@@ -124,5 +132,5 @@ object Writers extends App {
   val logs1 = sumFuture1.map(_.written) // logs from thread 1
   val logs2 = sumFuture2.map(_.written) // logs from thread 2
 
-  // benefit #2: writers can keep logs separate on multiple threads
+  // benefit #2: writers can keep logs separate on multiple threads, and you can process them separately
 }
