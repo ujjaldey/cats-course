ff5d9774f6f5af874b0734f95d09cf6b6436fb4c
Ujjal Dey <ujjaldey@gmail.com>
2023-05-24 00:46:58 +0800

    Re: Applicative

--- src/main/scala/part4typeclasses/Applicatives.scala
+++ src/main/scala/part4typeclasses/Applicatives.scala
@@ -1,13 +1,14 @@
 package part4typeclasses
 
 object Applicatives extends App {
+  // Applicatives are the extension of Functors that will introduce the pure method (from monad)
   // Applicative = Functors + the pure method
 
   import cats.Applicative // Applicative extends Apply extends Functor
   // Applicative comes with the pure method i.e. it has ability to wrap a normal value into a wrapped value
   import cats.instances.list._
 
-  val listApplicative = Applicative[List] // higher kind of type like Monad
+  val listApplicative = Applicative[List] // higher kind of type like Functor and Monad
   val aList = listApplicative.pure(2) // List(2)
   println(aList)
 
@@ -28,9 +29,8 @@ object Applicatives extends App {
   println(aSweetList, aSweetOption)
   println("====")
 
-  // Monads extend Applicatives
-  // Applicatives extend Functors
-  // Functors are with map, Applicatives are with pure method, and Monads are with pure and flatMap
+  // Monads extend Applicatives (because they inherit the pure method)
+  // Applicatives extend Functors (Functors are with map, Applicatives are with pure method, and Monads are with pure and flatMap)
 
   // Applicatives are rarely used by themselves, because most Applicatives are actually a stronger type (mondas)
   // Applicatives are rarely used because most of the data structures are monadic values
@@ -45,17 +45,23 @@ object Applicatives extends App {
   println(aValidValue, aModifiedValidate)
 
   val validatedApplicative = Applicative[ErrorsOr]
+  // the compiler can construct an implicit applicative with my type where the pure method does what was done above (Validated.valid(43))
+  // and map method does exactly same as the normal map method does on the Validated type
+
+  // Validated is not monad
 
   // TODO: thought experiment
   // Applicative already has ap method, so no need to define here
-  //  def ap[W[_], B, T](wf: W[B => T])(wa: W[B]): W[T] = ??? // this is already implemented
+  // def ap[W[_], B, T](wf: W[B => T])(wa: W[B]): W[T] = ??? // this is already implemented
 
   def productWithApplicative[W[_], A, B](wa: W[A], wb: W[B])(implicit applicative: Applicative[W]): W[(A, B)] = { // W=wrapper
     val functionWrapper: W[B => (A, B)] = applicative.map(wa)(a => (b: B) => (a, b))
     applicative.ap(functionWrapper)(wb)
+    // functionWrapper converts A to (A,B) (B and T in above definition of ap)
+    // wb is a wrapper of B
   }
 
   // Applicatives have this ap[W[_], B, T](wf: W[B => T])(wa: W[B]): W[T]
   // Applicatives can implement product from Semigroupal
-  // Applicatives extends Semigroupal
+  // Applicatives extends Semigroupal (i.e. it can define a product method in presence of this ap method)
 }
