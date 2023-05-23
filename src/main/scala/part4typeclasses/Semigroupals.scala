528e3577ab6a0e5eb7788306574e0ba784138769
Ujjal Dey <ujjaldey@gmail.com>
2023-05-23 22:58:16 +0800

    Re: Semigroupal

--- src/main/scala/part4typeclasses/Semigroupals.scala
+++ src/main/scala/part4typeclasses/Semigroupals.scala
@@ -5,15 +5,17 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object Semigroupals extends App {
   trait MySemigroupal[F[_]] {
-    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
+    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] // fundamental method of Semigroupal
   }
 
-  import cats.Semigroupal
-  import cats.instances.option._ // implicit Semigroupal[Option]
+  // the purpose of Semigroupal is to unwrap and wrap the items back, same as the case of Semigroup
 
-  val optionSemigroupal = Semigroupal[Option]
+  import cats.Semigroupal
+  import cats.instances.option._ // imports implicit Semigroupal[Option]
+
+  val optionSemigroupal = Semigroupal[Option] // uses implicit Semigroupal[Option] from cats.instances.option._
   val aTupledOption = optionSemigroupal.product(Some(123), Some("a string")) // Some(123, "a string")
-  val aNoneTupled = optionSemigroupal.product(Some(123), None) // None
+  val aNoneTupled = optionSemigroupal.product(Some(123), None) // None - if any one item is None, the result will be None
 
   println(aTupledOption)
   println(aNoneTupled)
@@ -26,15 +28,16 @@ object Semigroupals extends App {
 
   import cats.instances.list._ // implicit Semigroupal[List]
 
-  val aTupledList = Semigroupal[List].product(List(1, 2), List("a", "b")) // returns the List of the cartesian product of 1,2 and a,b
+  val aTupledList = Semigroupal[List].product(List(1, 2), List("a", "b")) // returns the List of the cartesian product of 1,2 and a,b (instead of zipping the result)
   println(aTupledList)
 
   // TODO: implement product with monad
 
   import cats.Monad
   import cats.syntax.flatMap._
-  import cats.syntax.functor._ // for flatMap
+  import cats.syntax.functor._ // extension methods for flatMap applicable to monad
 
+  // the fundamental methods for monads are pure, map, and flatMap
   def productWithMonads[F[_], A, B](fa: F[A], fb: F[B])(implicit monad: Monad[F]): F[(A, B)] =
     monad.flatMap(fa)(a => monad.map(fb)(b => (a, b)))
 
@@ -48,7 +51,7 @@ object Semigroupals extends App {
   println(productWithMonadsFor(List(1, 2), List("e", "f")))
 
   // from Monads.scala
-  // as we can safely define the product method from map and flatMap, we can say MyMonad can safely extend from MySemigroupal
+  // as we can safely define the product method from map and flatMap, we can say MyMonad can safely extend MySemigroupal
   trait MyMonad[M[_]] extends MySemigroupal[M] {
     def pure[A](value: A): M[A]
 
@@ -67,14 +70,16 @@ object Semigroupals extends App {
   // why semigroupals are important when monads can do cartesian product just fine -
   // monad product follow a for comprehension which is a sequence of map and flatMap and maps/flatMaps obey the so-called monad laws
   // these monad laws are there to impose the sequencing of operations, whereas we might want to combine value without the need of imposing sequencing of evaluation.
-  // usecase for semigroupals is Validated
+
+  // Validated is a good use case for Semigroupals
+  // Validated can be used to combining instances of type Validated without needing to follow the monad laws
 
   import cats.data.Validated
 
   type ErrorsOr[T] = Validated[List[String], T]
   val validatedSemigroupal = Semigroupal[ErrorsOr] // requires the implicit Semigroup[List[_]] which we can provide with import cats.instances.list._
-  // and we are able to combine the instances of Validated with the product method of the Semigroupal without needing to follow the sequencing of monad laws
 
+  // and we are able to combine the instances of Validated with the product method of the Semigroupal without needing to follow the sequencing of monad laws
   val invalidsCombination = validatedSemigroupal.product(
     Validated.invalid(List("something wrong", "something else wrong")),
     Validated.invalid(List("this can't be right"))
@@ -84,12 +89,12 @@ object Semigroupals extends App {
   // so Validated would combine through the combination function of the error type
   println(invalidsCombination)
 
-  // you wont get the same result using the monadic type like either
+  // you wont get the same result using the monadic type like Either
   type EitherErrorsOr[T] = Either[List[String], T]
 
   import cats.instances.either._ // import Monad[Either]
 
-  val eitherSemigroupal = Semigroupal[EitherErrorsOr] // Seimgroupal of type EitherErrorsOr in scope
+  val eitherSemigroupal = Semigroupal[EitherErrorsOr] // Semigroupal of type EitherErrorsOr in scope
 
   val eitherCombination = eitherSemigroupal.product( // Semigroupal[EitherErrorsOr] will be the Monad[EitherErrorOr]. so the product method will be implemented in terms of map and flatMap
     Left(List("something wrong", "something else wrong")),
@@ -103,7 +108,7 @@ object Semigroupals extends App {
 
   // TODO 2: define a Semigroupal[List] which does a zip (instead of product)
   val zipListSemigroual: Semigroupal[List] = new Semigroupal[List] {
-    override def product[A, B](fa: List[A], fb: List[B]): List[(A, B)] = fa.zip(fb)
+    override def product[A, B](listA: List[A], listB: List[B]): List[(A, B)] = listA.zip(listB)
   }
   println(zipListSemigroual.product(List(3, 4), List("x", "y")))
 }
