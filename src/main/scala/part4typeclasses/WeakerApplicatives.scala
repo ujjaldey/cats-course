0f17a66f94eb1f207cd16e619618b4952d32ec47
Ujjal Dey <ujjaldey@gmail.com>
2023-05-24 23:19:11 +0800

    Re: Weaker Applicatives: Apply

--- src/main/scala/part4typeclasses/WeakerApplicatives.scala
+++ src/main/scala/part4typeclasses/WeakerApplicatives.scala
@@ -6,7 +6,7 @@ object WeakerApplicatives extends App {
   // Cats have a Apply trait with product and ap methods.
   trait MyApply[W[_]] extends Functor[W] with Semigroupal[W] {
     // the product method can be implemented by using the map and ap methods
-    override def product[A, B](fa: W[A], fb: W[B]) = {
+    override def product[A, B](fa: W[A], fb: W[B]): W[(A, B)] = {
       val functionWrapper: W[B => (A, B)] = map(fa)(a => (b: B) => (a, b))
       ap(functionWrapper)(fb)
     }
@@ -15,7 +15,7 @@ object WeakerApplicatives extends App {
     def mapN[A, B, C](tuple: (W[A], W[B]))(f: (A, B) => C): W[C] = {
       val tupleWrapper = product(tuple._1, tuple._2) // wrapper over tuple of A, B: W[(A,B)]
       map(tupleWrapper) { // MyApply extends Functor, so it has access to map method
-        case (a, b) => f(a, b) // returns the C from the function f
+        case (a, b) => f(a, b) // returns the C from the function f(A, B)
       }
     }
 
@@ -42,14 +42,14 @@ object WeakerApplicatives extends App {
 
   import cats.syntax.apply._ // extension methods from Apply
 
-  val tupleOptions = (Option(1), Option(2), Option(3))
-  val optionOfTuple = tupleOptions.tupled // Some(1,2,3) // tupled unwrap the values of all these options, and the values are tupled and then wrapped back
+  val tupleOfOptions = (Option(1), Option(2), Option(3))
+  val optionOfTuple = tupleOfOptions.tupled // Some(1,2,3) // tupled unwrap the values of all these options, and the values are tupled and then wrapped back. this is by virtue of the presence of the product method
   println(optionOfTuple)
   // this is quite general. if you have tuple of Futures, you can convert it to a tuple containing all 3 values
   // this can be generalized for any type of wrapper type for which you have apply in scope.
   // can be used for Future as well
 
   // you can also apply a function on those values without needing to unwrap the type all the time
-  val sumOption = tupleOptions.mapN(_ + _ + _) // Some(6)
+  val sumOption = tupleOfOptions.mapN(_ + _ + _) // Some(6)
   // tupled and mapN are valid for any tuples arity. they can be applied to tuples with up to 22 elements
 }
