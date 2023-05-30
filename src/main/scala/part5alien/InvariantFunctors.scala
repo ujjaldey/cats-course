b54d41403f951053a91aae24315d6bc463fdf42c
Ujjal Dey <ujjaldey@gmail.com>
2023-05-31 00:34:51 +0800

    Re: Invariant

--- src/main/scala/part5alien/InvariantFunctors.scala
+++ src/main/scala/part5alien/InvariantFunctors.scala
@@ -2,6 +2,7 @@ package part5alien
 
 import cats.Monoid
 
+// Invariant Functor concepts have nothing to do with generic type variant
 object InvariantFunctors extends App {
   trait Crypto[A] {
     self =>
@@ -29,6 +30,7 @@ object InvariantFunctors extends App {
 
   println(encrypt("hijibiji"))
   println(decrypt[String](encrypt("hijibiji"))) // [String] is required
+  println("====")
 
   /*
     how can we support: Int, Double, Option[String] on this caesarCypher logic
@@ -41,15 +43,19 @@ object InvariantFunctors extends App {
   implicit val optionStringCrypto: Crypto[Option[String]] = caesarCypher.imap(_.getOrElse(""), Option(_))
   println(encrypt(Option("hijibiji"))) // Some won't work
   println(decrypt[Option[String]](encrypt(Option("hijibiji"))))
+  println("====")
 
   // TODO 2 - generalize this pattern. if you have a Crypto[T] => Crypto[Option[T]] if you have a Monoid[T] in scope
   implicit def optionCrypto[T](implicit crypto: Crypto[T], monoid: Monoid[T]): Crypto[Option[T]] =
     crypto.imap(_.getOrElse(monoid.empty), Option(_))
 
-  import cats.instances.double._
+  import cats.instances.double._ // imports Monoid[Double]
 
   println(encrypt(Option(Math.PI))) // also require cats.instances.double._ for Monoid[Double]
   println(decrypt[Option[Double]](encrypt(Option(Math.PI)))) // also require cats.instances.double._ for Monoid[Double]
+  println("====")
+
+  // same as above, but using curried form
 
   import cats.instances.string._
   import cats.{Invariant, Show} // Show[String]
@@ -64,17 +70,21 @@ object InvariantFunctors extends App {
   val showOptionString2: Show[Option[String]] = showString.imap(Option(_))(_.getOrElse(""))
 
   // TODO - what's the relationship?
-  trait MyInvariant[W[_]] { // "invariant" functor
+  // if, e.g., MyInvariant extends MyContraVariant, that means MyInvariant is stronger than MyContraVariant
+  // that means we can implement the weaker methods in terms of the stronger methods
+
+  // MyInvariant is the super type for both MyContraVariant and MyFunctor
+  trait MyInvariant[W[_]] { // "invariant" functor - can transform back and forth, i.e. has both co and contravariant capabilities. has imap
     def imap[A, B](wa: W[A])(forth: A => B)(back: B => A): W[B]
   }
 
-  trait MyContraVariant[W[_]] extends MyInvariant[W] { // "contravariant" functor
+  trait MyContraVariant[W[_]] extends MyInvariant[W] { // "contravariant" functor. has contramap
     def contramap[A, B](wa: W[A])(back: B => A): W[B]
 
     override def imap[A, B](wa: W[A])(forth: A => B)(back: B => A): W[B] = contramap(wa)(back)
   }
 
-  trait MyFunctor[W[_]] extends MyInvariant[W] { // "covariant" functor
+  trait MyFunctor[W[_]] extends MyInvariant[W] { // "covariant" functor. has map
     def map[A, B](wa: W[A])(forth: A => B): W[B]
 
     override def imap[A, B](wa: W[A])(forth: A => B)(back: B => A): W[B] = map(wa)(forth)
