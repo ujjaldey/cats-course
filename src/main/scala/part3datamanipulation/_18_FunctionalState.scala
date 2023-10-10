45d52f8351a199d0070df305385aaeb0594036ac
Ujjal Dey <ujjaldey@gmail.com>
2023-10-11 00:21:36 +0800

    Repeat: State

--- src/main/scala/part3datamanipulation/_18_FunctionalState.scala
+++ src/main/scala/part3datamanipulation/_18_FunctionalState.scala
@@ -1,7 +1,11 @@
 package part3datamanipulation
 
-object FunctionalState extends App {
+object _18_FunctionalState extends App {
+  // in functional programming, we don't use variables and mutations.
+
   type MyState[S, A] = S => (S, A)
+  // S is the type for the State, A is the answer or desirable value we obtain from a single computation
+  // so in above, from a State S, we obtain another State S of same type and the result of the single computation
 
   import cats.data.State
 
@@ -11,6 +15,8 @@ object FunctionalState extends App {
 
   // state is an abstraction for iterative computation expressed in purely functional terms
 
+
+  // bad code --
   // iterative
   var a = 10
   a += 1
@@ -19,13 +25,18 @@ object FunctionalState extends App {
   val secondComputation = s"Multiplied with 5, obtained $a"
   println(a)
 
+  println("====1")
+
   // pure FP with states
   val firstTransformation = State((s: Int) => (s + 1, s"Added 1 to 10, obtained ${s + 1}"))
   val secondTransformation = State((s: Int) => (s * 5, s"Multiplied with 5, obtained ${s * 5}"))
   val compositeTransformation: State[Int, (String, String)] = firstTransformation.flatMap { firstResult =>
     secondTransformation.map(secondResult => (firstResult, secondResult))
   }
-  println(compositeTransformation.run(10).value)
+
+  println(compositeTransformation.run(10).value) // calculates the state value 55 and the computation result
+
+  println("====2")
 
   // for comprehension
   val compositeTransformation2: State[Int, (String, String)] = for {
@@ -35,22 +46,29 @@ object FunctionalState extends App {
 
   println(compositeTransformation2.run(10).value)
 
+  println("====2")
+
   // by doing via function chaining instead of State, will deeply nest the result
   // also for every chain, you have to implement decomposition logic
   val funct1 = (s: Int) => (s + 1, s"Added 1 to 10, obtained ${s + 1}")
   val funct2 = (s: Int) => (s * 5, s"Multiplied with 5, obtained ${s * 5}")
 
-  val compositeFunc = funct1.andThen {
+  val compositeFunc = funct1.andThen { // andThen is the natural function sequence
     case (newState, firstResult) => (firstResult, funct2(newState))
+    // andThen takes the output of the first function which is a tuple, so we have to decompose it into newState, firstResult
+    // and then return the tuple of the first and second result
   }
   println(compositeFunc(10)) // value 55 is nested inside the 2nd call
+  // if we have to chain the function again, we need to decompose 3 values - which is very clunky
+  // hence using State is elegant
   println("====")
 
   // TODO: an online store
   case class ShoppingCart(items: List[String], total: Double)
 
-  def addToCart(item: String, price: Double): State[ShoppingCart, Double] = State { cart =>
-    (ShoppingCart(item :: cart.items, cart.total + price), price + cart.total)
+  def addToCart(item: String, price: Double): State[ShoppingCart, Double] = State { cart => // apply factory method
+    (ShoppingCart(item :: cart.items, cart.total + price), cart.total + price)
+    // the update Cart will have the newly added items and the price, and also return the total price of all the products
   }
 
   val udsCart: State[ShoppingCart, Double] = for {
@@ -63,10 +81,11 @@ object FunctionalState extends App {
   println("====")
 
   // TODO 2: pure mental gymnastics
+
   // returns a State data structure that, when run, will not change the state but will issue the value f(a)
   def inspect[A, B](f: A => B): State[A, B] = State((a: A) => (a, f(a)))
 
-  // returns a State data structure that, when run, returns the value of that state and makes no changes
+  // returns a State data structure that, when run, returns the exact value of that state and makes no changes
   def get[A]: State[A, A] = State((a: A) => (a, a))
 
   // returns a State data structure that, when run, returns Unit and sets the state to that value
@@ -75,14 +94,15 @@ object FunctionalState extends App {
   // returns a State data structure that, when run, will return Unit and sets the state to f(state)
   def modify[A](f: A => A): State[A, Unit] = State((a: A) => (f(a), ()))
 
-  // all the 4 above methods are already available in the import: import cats.data.State._
+  // all the 4 above methods are already available in the companion object of State:
+  // import: import cats.data.State._
 
   val program: State[Int, (Int, Int, Int)] = for {
-    a <- get[Int]
-    _ <- set[Int](a + 10) // returns ()
-    b <- get[Int]
-    _ <- modify[Int](_ + 43) // returns ()
-    c <- inspect[Int, Int](_ * 2)
+    a <- get[Int] // does not modify the State, bur returns value as 5
+    _ <- set[Int](a + 10) // State is 5+10=15, returns ()
+    b <- get[Int] // does not modify the State, bur returns value as 15
+    _ <- modify[Int](_ + 43) // State is 15+43=58, returns ()
+    c <- inspect[Int, Int](_ * 2) // State is 58, but returns 58*2=116
   } yield (a, b, c)
 
   println(program.run(5).value) // the value is 58, as modify() updates the state value to 15+43. inspect does not change the state value
