4c4cdce1ede9684df653b4010879096c8cc53c6e
Ujjal Dey <ujjaldey@gmail.com>
2023-10-12 00:55:41 +0800

    Repeat: Data Validation, Part 2

--- src/main/scala/part3datamanipulation/_19_DataValidation.scala
+++ src/main/scala/part3datamanipulation/_19_DataValidation.scala
@@ -5,29 +5,30 @@ import cats.Semigroup
 import scala.annotation.tailrec
 import scala.util.Try
 
-object DataValidation extends App {
+object _19_DataValidation extends App {
 
+  println("==== PART 1 ====")
+
+  // encapsulate data validation logic and the errors
   // acts like Either. Left side is undesirable, right side is desirable
 
   import cats.data.Validated
 
-  val aValidValue: Validated[String, Int] = Validated.valid(42) // Right value (Int) in Either
+  val aValidValue: Validated[String, Int] = Validated.valid(42) // Right value (Int) of Either
   val anInvalidValue: Validated[String, Int] = Validated.invalid("something went wrong") // Left value (String) of Either
 
   // cond has 3 params: a predicate with boolean condition, value if condition is true, value if condition is false
   val aTest: Validated[String, Int] = Validated.cond(42 > 39, 99, "meaning of life is too small")
 
-  // Validated has a different contract to Either. Validated can combine all the errors into one giant value by no mutation, just by purely functional programming
+  // Validated acts like Either - undesirable value on the left side, and the desirable value on the right side
+  // then why can't we use Either instead - Validated has a different contract to Either.
+  // Validated can combine all the errors into one giant value by no mutation, just by purely
+  // functional programming
   println(aTest)
   println("====")
 
   // TODO: use Either
-  /*
-    - n must be a prime
-    - n must be non-negative
-    - n <= 100
-    - n must be even
-   */
+
   def testPrime(n: Int) = {
     @tailrec
     def tailRecPrime(d: Int): Boolean =
@@ -38,8 +39,14 @@ object DataValidation extends App {
     else tailRecPrime(Math.abs(n / 2))
   }
 
-  // elegant, but clunky. not good
+  /*
+    - n must be a prime
+    - n must be non-negative
+    - n <= 100
+    - n must be even
+   */
 
+  // elegant, but clunky. not good
   def testNumber(n: Int): Either[List[String], Int] = {
     val isNotEven: List[String] = if (n % 2 == 0) List() else List("Number must be even")
     val isNegative: List[String] = if (n >= 0) List() else List("Number must be non-negative")
@@ -54,19 +61,23 @@ object DataValidation extends App {
   println(testNumber(20))
   println(testNumber(-20))
   println(testNumber(201))
-  println("====")
+  println("=======")
 
   // using Validated - superior
 
   import cats.instances.list._ // appropriate combination function for list
 
-  implicit val combineIntMax: Semigroup[Int] = Semigroup.instance[Int](Math.max) // also require the implicit Semigroup for Int
+  // implicit Semigroup for Int
+  implicit val combineIntMax: Semigroup[Int] = Semigroup.instance[Int](Math.max)
 
   def validateNumber(n: Int): Validated[List[String], Int] =
     Validated.cond(n % 2 == 0, n, List("Number must be even"))
-      .combine(Validated.cond(n >= 0, n, List("Number must be non-negative"))) // require import of List semigroup and implicit of Int Semigroup
+      .combine(Validated.cond(n >= 0, n, List("Number must be non-negative")))
       .combine(Validated.cond(n <= 100, n, List("Number must be less than or equal to 100")))
       .combine(Validated.cond(testPrime(n), n, List("Number must be a prime")))
+  // the combine() method above requires semigroup of List[String] and Int
+  // as the combine would require the default implicit combination function of both the left hand side
+  // and right hand side of the Validated instance
 
   println(validateNumber(2))
   println(validateNumber(20))
@@ -75,40 +86,52 @@ object DataValidation extends App {
 
   println("==== PART 2 ====")
 
+  // Validated instances can be chained with a function that looks like flatMap called andThen
+
   // chain
   // andThen converts a valid value to another Validated value of different type
-  // however, andThen is not flatMap, as flatMap will not continue with the transformation and accumulate the error in case the original value is invalid
-  // flatMap will short-circuit the additional chain. hence we do not use flatMap
+
+  // however, andThen is not flatMap, as flatMap will not continue with the transformation
+  // and accumulate the error in case the original value is invalid.
+  // flatMap will short-circuit the additional chain - which we do not want.
+  // hence we do not use flatMap
+
   // Validated is not applicable for Monad
   println(aValidValue.andThen(_ => anInvalidValue))
 
-  // test valid value
-  // if the predicate (boolean function) in ensure() fails from the wrapped value of the valid value, then this will turn the valid value into invalid value where the invalid error is described from the first argument "something went wrong"
-  println("valid: ", aValidValue.ensure(List("something went wrong"))(_ % 2 == 0))
+  // test valid value - ensure
+  // ensure() takes an  predicate (boolean function) and if the predicate fails from the wrapped value
+  // of the valid value, then this will turn the valid value into invalid value where the invalid error
+  // is described from the first argument "something went wrong"
+  println("valid: ", aValidValue.ensure(List("something went wrong"))(_ % 2 == 0)) // _ is 42
   println("invalid: ", aValidValue.ensure(List("something went wrong"))(_ % 2 == 1))
+
   // transform
   println(aValidValue.map(_ + 1)) // map on the right value i.e. valid value
   println(aValidValue.leftMap(_.length)) // leftMap: map on the left value i.e. error value
   println(anInvalidValue.leftMap(_.length))
-  println(aValidValue.bimap(_.length, _ + 1)) // map on both - valid value and error type
+  println(aValidValue.bimap(_.length, _ + 1)) // map on both - error type and the valid value
   println(anInvalidValue.bimap(_.length, _ + 1))
   println("====~~")
 
   // interoperate with stdlib - Either, Option, Try
   val eitherToValidated: Validated[List[String], Int] = Validated.fromEither(Right(42))
   val optionToValidated: Validated[List[String], Int] = Validated.fromOption(None, List("nothing present here"))
-  val tryToValidated: Validated[Throwable, Int] = Validated.fromTry(Try("something".toInt)) // "something".toInt will throw exception
+  // fromOption takes 2 arguments - Option[Something] and the error value if the Option is empty (None)
+  val tryToValidated: Validated[Throwable, Int] = Validated.fromTry(Try("something".toInt))
+  // "something".toInt will throw exception. That Throwable (actual JVM exception) would be the error type of the Validated
 
+  // you can store anything that means an error to you. That's the power of Validated. And also, you can combine them.
   println(eitherToValidated)
   println(optionToValidated)
   println(tryToValidated)
-  println("====")
+  println("=======")
 
-  // backwards
+  // backwards - Validated to Either, Option, Try
   println(aValidValue.toOption)
   println(aValidValue.toEither)
   // nothing like toTry, because Validated has to be typed with a throwable
-  println("====")
+  println("====~~~~~~")
 
   // TODO 2 - form validation
   object FormValidation {
@@ -119,6 +142,7 @@ object DataValidation extends App {
 
     def getValue(form: Map[String, String], fieldName: String): FormValidation[String] = {
       // get() of Map will return an Option. If Option is None, then it returns the 2nd argument with List[String]
+      // If Option has a value, then it's a valid scenario and will return the value of the field
       Validated.fromOption(form.get(fieldName), List(s"The field $fieldName must be specified."))
     }
 
@@ -141,13 +165,16 @@ object DataValidation extends App {
         - name, email, and password MUST be specified
         - name must not be blank
         - email must have "@"
-        - password must have >= characters
+        - password must have >= 10 characters
      */
     def validateForm(form: Map[String, String]): FormValidation[String] =
+    // get the value first, then chain using andThen and validate the extracted value from getValue()
       getValue(form, "Name").andThen(name => nonBlank(name, "Name")) // andThen is similar to flatMap
         .combine(getValue(form, "Email").andThen(emailProperForm)) // requires import cats.instances.string._
         .combine(getValue(form, "Password").andThen(passwordCheck)) // requires import cats.instances.string._
-        .map(_ => "User registration complete.")
+        .map(_ => "User registration complete.") // show success when all validations are success
+
+    // the above combine function would require to import cats.instances.string._ for the natural string concatenation
   }
 
   val form = Map(
@@ -168,9 +195,9 @@ object DataValidation extends App {
   )))
   println("====")
 
-  import cats.syntax.validated._ // extension method for nicer api
+  import cats.syntax.validated._ // Cats Validated has some extension methods for nicer api
 
   println(42.valid[List[String]]) // Validated[List[String], Int]
-  println("something wrong".invalid[List[String]]) // Validated[List[String], Int]
+  println("something wrong".invalid[Int]) // Validated[String, Int]
   println("====")
 }
